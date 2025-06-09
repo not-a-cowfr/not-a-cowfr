9d6d8bec205686bcad5f6ebac76842003423c90d
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-06-09 09:33:07 -0700

    move js snippets to md file

--- configs/legcord/snippets/plugins_keybind.js
+++
@@ -1,9 +0,0 @@
-// ctrl + shift + x to open plugins settings, like vscode
-document.addEventListener("keydown", function (event) {
-  if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === "X") {
-    Vencord.Webpack.findByProps("saveAccountChanges", "open").open(
-      "EquicordSettings",
-      null,
-    );
-  }
-});
