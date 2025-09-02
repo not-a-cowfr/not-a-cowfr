8762d8625d8be78ec28ef5601a74d1426a10cf26
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-09-02 10:34:36 -0700

    add minecraft server

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -67,6 +67,64 @@
   # openssh
   services.openssh.enable = true;
 
+  # minecraft server
+  # services.minecraft-servers = {
+  #   enable = true;
+  #   eula = true;
+  #   openFirewall = true;
+
+  #   # for seperate mods
+  #   # servers.fabric = {
+  #   #   enable = true;
+
+  #   #   package = pkgs.fabricServers.fabric-1_21_8.override {
+  #   #     loaderVersion = "0.17.2";
+  #   #   };
+
+  #   #   symlinks = {
+  #   #     mods = pkgs.linkFarmFromDrvs "mods" (
+  #   #       builtins.attrValues {
+  #   #         Fabric-API = pkgs.fetchurl {
+  #   #           url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/9YVrKY0Z/fabric-api-0.115.0%2B1.21.8.jar";
+  #   #           sha512 = "e5f3c3431b96b281300dd118ee523379ff6a774c0e864eab8d159af32e5425c915f8664b1cd576f20275e8baf995e016c5971fea7478c8cb0433a83663f2aea8";
+  #   #         };
+  #   #         Backpacks = pkgs.fetchurl {
+  #   #           url = "https://cdn.modrinth.com/data/MGcd6kTf/versions/Ci0F49X1/1.2.1-backpacks_mod-1.21.2-1.21.3.jar";
+  #   #           sha512 = "6efcff5ded172d469ddf2bb16441b6c8de5337cc623b6cb579e975cf187af0b79291b91a37399a6e67da0758c0e0e2147281e7a19510f8f21fa6a9c14193a88b";
+  #   #         };
+  #   #       }
+  #   #     );
+  #   #   };
+  #   # };
+
+  #   # for modpack
+  #   let
+  #     modpack = pkgs.fetchPackwizModpack {
+  #       url = "";
+  #       packHash = "";
+  #     };
+  #     mcVersion = modpack.manifest.versions.minecraft;
+  #     fabricVersion = modpack.manifest.versions.fabric;
+  #     serverVersion = lib.replaceStrings [ "." ] [ "_" ] "fabric-${mcVersion}";
+  #   in
+  #   {
+  #   cool-modpack = {
+  #     enable = true;
+  #     package = pkgs.fabricServers.${serverVersion}.override { loaderVersion = fabricVersion; };
+  #     symlinks = {
+  #       "mods" = "${modpack}/mods";
+  #     };
+  #     files = {
+  #       "config" = "${modpack}/config";
+  #       # "config/mod1.yml" = "${modpack}/config/mod1.yml";
+  #       # "config/server-specific.conf".value = {
+  #       #   example = "foo-bar";
+  #       # };
+  #     };
+  #   };
+  # };
+  # };
+
   # virtualisation
   virtualisation.docker.enable = true;
 
