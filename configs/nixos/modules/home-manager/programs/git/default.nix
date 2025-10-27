bd31e02efcecad3642e774be6f440a843b5cd430
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-10-26 22:49:36 -0700

    update git settings

--- configs/nixos/modules/home-manager/programs/git/default.nix
+++ configs/nixos/modules/home-manager/programs/git/default.nix
@@ -2,13 +2,18 @@
 {
   programs.git = {
     enable = true;
-    userName = "not a cow";
-    userEmail = "104255555+not-a-cowfr@users.noreply.github.com";
+
     # signing = {
     #   key = userConfig.gitKey;
     #   signByDefault = true;
     # };
-    extraConfig = {
+
+    settings = {
+      user = {
+        name = "not a cow";
+        email = "104355555+not-a-cowfr@users.noreply.github.com";
+      };
+
       pull.rebase = "true";
       init.defaultBranch = "main";
       credential.helper = "store";
