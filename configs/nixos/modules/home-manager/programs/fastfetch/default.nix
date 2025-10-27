4a4a6f470f51bde105d2e3dfd297c80253fc7f46
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-10-26 22:47:32 -0700

    update fastfetch
    for some reason the pan image is fucking giant and i have no idea why

--- configs/nixos/modules/home-manager/programs/fastfetch/default.nix
+++ configs/nixos/modules/home-manager/programs/fastfetch/default.nix
@@ -3,111 +3,114 @@
   programs.fastfetch = {
     enable = true;
     settings = {
-      # logo = "~/.config/fastfetch/ferris.txt";
+      logo = {
+        source = "/etc/nixos/modules/home-manager/programs/fastfetch/nixos.png";
+      };
 
       display = {
         separator = " ➜  ";
       };
 
       modules = [
+        "break"
+        "break"
         {
           type = "os";
-          key = " OS        ";
+          key = "OS        ";
           keyColor = "red";
         }
         {
           type = "kernel";
-          key = " ├ kernel  ";
+          key = "├ kernel  ";
           keyColor = "red";
         }
         {
           type = "packages";
-          key = " ├ packages";
+          key = "├ packages";
           keyColor = "red";
         }
         {
           type = "shell";
-          key = " └ shell   ";
+          key = "└ shell   ";
           keyColor = "red";
         }
         "break"
         {
           type = "wm";
-          key = " WM        ";
+          key = "WM        ";
           keyColor = "green";
         }
         {
           type = "wmtheme";
-          key = " ├ theme   ";
+          key = "├ theme   ";
           keyColor = "green";
         }
         {
           type = "icons";
-          key = " ├ icons   ";
+          key = "├ icons   ";
           keyColor = "green";
         }
         {
           type = "cursor";
-          key = " ├ cursor  ";
+          key = "├ cursor  ";
           keyColor = "green";
         }
         {
           type = "terminal";
-          key = " ├ terminal";
+          key = "├ terminal";
           keyColor = "green";
         }
         {
           type = "terminalfont";
-          key = " └ font    ";
+          key = "└ font    ";
           keyColor = "green";
         }
         "break"
         {
           type = "host";
           format = "{5} {1} Type {2}";
-          key = " PC        ";
+          key = "PC        ";
           keyColor = "yellow";
         }
         {
           type = "cpu";
           format = "{1} ({3} cores) @ {7}";
-          key = " ├ cpu     ";
+          key = "├ cpu     ";
           keyColor = "yellow";
         }
         {
           type = "gpu";
-          format = "{1} {2} @ {12} GHz";
-          key = " ├ gpu     ";
+          format = "{1} {2} @ {12}";
+          key = "├ gpu     ";
           keyColor = "yellow";
         }
         {
           type = "memory";
-          key = " ├ memory  ";
+          key = "├ memory  ";
           keyColor = "yellow";
         }
         {
           type = "swap";
-          key = " ├ swap    ";
+          key = "├ swap    ";
           keyColor = "yellow";
         }
         {
           type = "disk";
-          key = " ├ disk    ";
+          key = "├ disk    ";
           keyColor = "yellow";
         }
         {
           type = "monitor";
           format = "{2}x{3} @ {11} Hz";
-          key = " ├ monitor ";
+          key = "├ monitor ";
           keyColor = "yellow";
         }
         {
           type = "custom";
           format = "7 maybe 7.2";
-          key = " └ weight  ";
+          key = "└ weight  ";
           keyColor = "yellow";
         }
-        "break"
         "break"
       ];
     };
