63555e239baf75d2e9cb41c3c44f26a2c0cdc840
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-10-27 12:09:36 -0700

    move panel to seperate folder

--- configs/nixos/modules/home-manager/desktop/kde/default.nix
+++ configs/nixos/modules/home-manager/desktop/kde/default.nix
@@ -36,7 +36,6 @@ in
     kdotool
     tela-circle-icon-theme
     otto
-    plasmusic-toolbar
   ];
 
   services.gpg-agent = {
@@ -165,105 +164,7 @@ in
     overrideConfig = true;
 
     panels = [
-      {
-        location = "bottom";
-        widgets = [
-          "org.kde.plasma.kickoff"
-          "org.kde.plasma.marginsseparator"
-          {
-            name = "org.dhruv8sh.kara";
-            config = {
-              general = {
-                animationDuration = 75;
-                # highlightType = 1;
-                type = 3;
-              };
-            };
-          }
-          "org.kde.plasma.panelspacer"
-          {
-            iconTasks = {
-              launchers = [
-                "applications:systemsettings.desktop"
-                "applications:org.kde.dolphin.desktop"
-
-                "org.kde.plasma.marginsseparator"
-
-                "applications:com.mitchellh.ghostty.desktop"
-                "applications:github-desktop.desktop"
-                "applications:codium.desktop"
-
-                "org.kde.plasma.marginsseparator"
-
-                "applications:legcord.desktop"
-                "applications:spotify.desktop"
-                "preferred://browser"
-
-                "org.kde.plasma.marginsseparator"
-
-                "applications:org.prismlauncher.PrismLauncher.desktop"
-                "applications:steam.desktop"
-              ];
-            };
-          }
-          "org.kde.plasma.panelspacer"
-          {
-            name = "plasmusic-toolbar";
-            config = {
-              general = {
-                useCustomFont = true;
-                customFont = "ComicShannsMono Nerd Font,10,-1,5,700,0,0,0,0,0,0,0,0,0,0,1";
-
-                titlePosition = 2;
-                skipBackwardControlInPanel = false;
-                skipForwardControlInPanel = false;
-                playPauseControlInPanel = false;
-
-                panelIconSizeRatio = 1;
-                useAlbumCoverAsPanelIcon = true;
-                maxSongWidthInPanel = 300;
-
-                desktopWidgetBg = 0;
-                fullAlbumCoverAsBackground = true;
-              };
-            };
-          }
-          "org.kde.plasma.marginsseparator"
-          {
-            systemTray.items = {
-              shown = [
-                "org.kde.plasma.volume"
-                "org.kde.plasma.battery"
-                "org.kde.plasma.bluetooth"
-                "org.kde.plasma.networkmanagement"
-              ];
-
-              hidden = [
-                "polychromatic-tray-applet"
-                "spotify-client"
-                "org.kde.kscreen"
-                "org.kde.kdeconnect"
-                "org.kde.plasma.clipboard"
-                "org.kde.plasma.cameraindicator"
-                "org.kde.plasma.devicenotifier"
-                "org.kde.plasma.keyboardlayout"
-                "org.kde.plasma.keyboardindicator"
-                "org.kde.plasma.manage-inputmethod"
-                "org.kde.plasma.mediacontroller"
-                "org.kde.plasma.weather"
-                "org.kde.plasma.brightness"
-              ];
-            };
-          }
-          {
-            digitalClock = {
-              time.format = "24h";
-              date.format = "isoDate";
-            };
-          }
-        ];
-        hiding = "normalpanel";
-      }
+      (import ./panels/windows-like.nix)
       # {
       #   location = "top";
       #   height = 26;
